5c2fabce103395a37300bf3767c25317e015f72f
Michael Chadwick <michael.chadwick@gmail.com>
2022-07-21 10:33:07 -0700

    added some linting configs and updated install script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # color variables
 C_FAILURE="\x1B[31m"
 C_SUCCESS="\x1B[32m"
+C_INFO="\x1B[33m"
 C_MISC="\x1B[34m"
 C_RESET="\x1B[0m"
 
@@ -22,8 +23,11 @@ function skip_file() {
   echo -e "${C_FAILURE}Skipped $1: $2${C_RESET}"
 }
 
-echo "Linking local $HOME/. files to custom _dotfiles..."
-echo "**********************************************"
+echo -e "${C_INFO}"
+echo "*****************************************************************"
+echo "Linking local '$HOME/.' files to custom _dotfiles..."
+echo "*****************************************************************"
+echo -e "${C_RESET}"
 
 # loop through template config and symlink to $HOME
 for src_file in _*; do
@@ -46,38 +50,72 @@ for src_file in _*; do
   fi
 done
 
+## LINTING
+for src_file in linting/_*; do
+  f="$(basename -- "$src_file")"
+  src_path="$PWD/$src_file"
+  dest_path="$HOME"/.${f:1}.json
+
+  if [ ! -e "$dest_path" ]; then
+    link_file "$src_path" "$dest_path"
+  else
+    if [ "$1" == "-d" ]; then
+      if [ -h "$dest_path" ]; then
+        skip_file "$dest_path" $err_linked
+      else
+        backup_file "$dest_path"
+        link_file "$src_path" "$dest_path"
+      fi
+    else
+      skip_file "$dest_path" $err_exists
+    fi
+  fi
+done
+
+echo -e "${C_INFO}"
+echo "*****************************************************************"
+echo "Creating missing '$HOME' dirs for git, ssh, vim, etc."
+echo "*****************************************************************"
+echo -e "${C_RESET}"
+
+## GIT
+# make .gitconfig.d dir if nonexistent
 if [ ! -d "$HOME"/.gitconfig.d ]; then
   mkdir "$HOME"/.gitconfig.d
 fi
 
-# set up vim color theme
+## SSH
+# make ssh dir if nonexistent
+# then link custom ssh config
+if [ ! -d "$HOME"/.ssh ]; then
+  mkdir -p "$HOME"/.ssh
+fi
+if [ ! -f "$HOME"/.ssh/config ]; then
+  ln -s "$PWD"/ssh/_config "$HOME"/.ssh/config
+fi
+
+## STARSHIP
+# make starship dir if nonexistent
+if [ -d .starship ]; then
+  if [ ! -d "$HOME"/.starship ]; then
+    mkdir -p "$HOME"/.starship
+  fi
+fi
+if [ ! -f "$HOME"/.starship/config.toml ]; then
+  ln -s "$PWD"/starship/_config.toml "$HOME"/.starship/config.toml
+fi
+
+## VIM
+# make .vim directory if nonexisting
+# add custom color theme
 if [ ! -d "$HOME"/.vim ]; then
   mkdir -p "$HOME"/.vim/colors
   cd "$HOME"/.vim/colors || exit
   wget https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim
 fi
 
-# make ssh dir if doesn't exist
-if [ ! -d "$HOME"/.ssh ]; then
-  mkdir -p "$HOME"/.ssh
-fi
-
-# then link custom ssh config
-if [ ! -f "$HOME"/.ssh/config ]; then
-  ln -s "$PWD"/ssh/_config "$HOME"/.ssh/config
-fi
-
-# make starship dir if doesn't exist
-if [ -d .starship ]; then
-  if [ ! -d "$HOME"/.starship ]; then
-    mkdir -p "$HOME"/.starship
-  fi
-fi
-
-# then link custom starship.rs config
-if [ ! -f "$HOME"/.starship/config.toml ]; then
-  ln -s "$PWD"/starship/_config.toml "$HOME"/.starship/config.toml
-fi
-
-echo "*********************************************"
-echo -e "Done!"
+echo -e "${C_INFO}"
+echo "*****************************************************************"
+echo "Done!"
+echo "*****************************************************************"
+echo -e "${C_RESET}"
